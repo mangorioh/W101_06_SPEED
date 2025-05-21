f2480adfd35635f54080cca51e239aa6a74c7499
qrq1356 <qrq1356@autuni.ac.nz>
2025-05-21 23:03:41 +1200

    user jwt login and register

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: true, credentials: true });
   dotenv.config();
   await app.listen(process.env.PORT ?? 3000);
 }
