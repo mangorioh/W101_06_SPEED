218a6eff45a823875d4cc9b4b519745adcf3b941
KugeznotKarbs <leonardkirbypascua@gmail.com>
2025-05-22 08:49:07 +1200

    feat: be-search-mongo-integration
    Updated CRUD operations for MongoDB

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Article } from './schemas/article.schema';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'SPEED BACKEND is running...';
+  constructor(@InjectModel(Article.name) private articleModel: Model<Article>) {}
+
+async findAll() {
+  return this.articleModel.find().select(
+    'title author published_date publisher status description isbn moderatedBy rating reason_for_decision volume number journal updated_date moderated_date'
+  ).lean().exec();
+}
+
+  async searchArticles(query: string) {
+    return this.articleModel.find({
+      $or: [
+        { title: new RegExp(query, 'i') },
+        { author: new RegExp(query, 'i') },
+        { publisher: new RegExp(query, 'i') }
+      ]
+    }).select('title author published_date publisher status reason_for_decision')
+    .exec();
   }
 }
