4cd283acb118392db35df793643749f9f4b6580a
KugeznotKarbs <leonardkirbypascua@gmail.com>
2025-05-22 08:50:29 +1200

    feat: be-search-mongo-integration
    Added CORS configuration and port changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  dotenv.config();
-  await app.listen(process.env.PORT ?? 3000);
+app.enableCors({
+  origin: 'http://localhost:3000', // Frontend URL
+  methods: 'GET,POST',
+  allowedHeaders: 'Content-Type,Authorization'
+});
+  await app.listen(4000); 
 }
 bootstrap();
