9f0d7cdf4d6581923e35045a42e78953d28875f4
qrq1356 <qrq1356@autuni.ac.nz>
2025-06-01 15:48:44 +1200

    testing

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken('User'),
+          useValue: {
+            findOne: jest.fn(),
+            create: jest.fn(),
+            // add other methods as needed
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
