d7b1db09b0cae31742f2c978b44cf213d05466da
mangorioh <104879428+mangorioh@users.noreply.github.com>
2025-06-02 20:07:26 +1200

    developed test steps

--- backend/src/ratings/rating.service.spec.ts
+++ backend/src/ratings/rating.service.spec.ts
@@ -1,10 +1,26 @@
 describe('RatingService (minimal unit test)', () => {
 
   it('should create a new rating (when none exists) and update article sums', async () => {
-    // TODO: write test that verifies behaviour when making new rating
+        // arrange
+        //   article with ratingSum = oldSum and ratingCount = oldCount
+        //   no existing rating for (articleId, userId)
+        //
+        // act
+        //   call createOrUpdateRating(userId, { articleId, value })
+        //
+        // assert
+        //   check new rating was created with the right value
+        //   check article.ratingSum went from oldSum to oldSum + value
+        //   check article.ratingCount went from oldCount to oldCount + 1
+        //   check averageRating is (oldSum + value) / (oldCount + 1)
   });
 
-  it('should throw exception if the article does not exist', async () => {
-    // TODO: write test that verifies intended behaviour on missing article
+  it('should throw NotFoundException if the article does not exist', async () => {
+    // arrange
+    //   pretend there’s no article for that articleId
+    //
+    // act & assert
+    //   calling createOrUpdateRating(userId, { articleId, value }) should throw notFoundException
+
   });
 });
