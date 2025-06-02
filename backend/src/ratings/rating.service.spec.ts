e2f0810d41d15b82b0d3054802c7a8fed6d70106
mangorioh <104879428+mangorioh@users.noreply.github.com>
2025-06-02 17:14:28 +1200

    test framework

---
+++ backend/src/ratings/rating.service.spec.ts
@@ -0,0 +1,10 @@
+describe('RatingService (minimal unit test)', () => {
+
+  it('should create a new rating (when none exists) and update article sums', async () => {
+    // TODO: write test that verifies behaviour when making new rating
+  });
+
+  it('should throw exception if the article does not exist', async () => {
+    // TODO: write test that verifies intended behaviour on missing article
+  });
+});
