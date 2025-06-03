e20470d06c4fea2298fa139b410a3a3715dd49c8
qrq1356 <qrq1356@autuni.ac.nz>
2025-06-03 18:39:08 +1200

    (backend): add auth restrictions to backend

--- backend/src/articles/article.controller.ts
+++ backend/src/articles/article.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './create-article.dto';
 import { UpdateArticleDto } from './update-article.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('articles')
 export class ArticleController {
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService) { }
 
+  @Roles('user', 'mod', 'owner')
   @Post()
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articleService.create(createArticleDto);
@@ -22,7 +27,7 @@ export class ArticleController {
   findOne(@Param('id') id: string) {
     return this.articleService.findById(id);
   }
-
+  @Roles('mod', 'owner')
   @Put(':id')
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articleService.update(id, updateArticleDto);
