e63fd7503321a3b7c30efdf60770ab1bb3ab0a14
mangorioh <104879428+mangorioh@users.noreply.github.com>
2025-06-04 18:21:13 +1200

    tdd tests

---
+++ backend/src/practices/practice.service.spec.ts
@@ -0,0 +1,13 @@
+describe('PracticeService', () => {
+  it('should rename a practice', () => {
+    // TODO: Implement renaming logic test
+  });
+
+  it('should update the valid flag on a practice', () => {
+    // TODO: Implement validity toggle test
+  });
+
+  it('should throw if practice not found', () => {
+    // TODO: Test not-found case
+  });
+});
